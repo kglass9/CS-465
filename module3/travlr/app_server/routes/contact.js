e5be04403afb5d19b9e09c0a69786c79f715aa5d
kglass9 <107563649+kglass9@users.noreply.github.com>
2023-09-24 14:18:45 -0400

    Delete module3/travlr directory

--- module3/travlr/app_server/routes/contact.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller= require('../controllers/contact');
-
-/* GET contact page. */
-router.get('/', controller.contact);
-
-module.exports = router;
