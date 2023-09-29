b14e44a8a549d953c7078906e6dfbdb551c3334f
Kari G <kiaraglass9@gmail.com>
2023-09-28 21:23:54 -0400

    module 4

--- module4/travlr/app_server/database/db.js
+++ module4/travlr/app_server/database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
+const host = process.env.DB_HOST || `127.0.0.1`;
 const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
@@ -11,27 +11,27 @@ const connect = () => {
         useNewUrlParser: true,
         useCreateIndex: true
     }), 1000);
-}
+};
 
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${dbURI}`);
 });
 
 mongoose.connection.on('error', err => {
-    console.log('Mongoose connection error:', err);
+    console.log(`Mongoose connection error:`, err);
 });   
 
 mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+    console.log(`Mongoose disconnected`);
 });   
 
-if (process.platform === 'win32'){
+if (process.platform === "win32"){
     const rl = readLine.createInterface ({
         input: process.stdin,
         output: process.stdout
     });
     rl.on ('SIGINT', () => {
-        process.emit ("SIGINT");
+        process.emit ('SIGINT');
     });
 }
 
@@ -45,18 +45,18 @@ const gracefulShutdown = (msg, callback) => {
 // For nodemon restarts
 process.once('SIGUSR2', () => {
     gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
+        process.kill(process.pid, "SIGUSR2");
     });
 });
 // For app termination
-process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
+process.on("SIGINT", () => {
+    gracefulShutdown("app termination", () => {
         process.exit(0);
     });
 });
 // FOor Heroku app termination
-  process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
+  process.on("SIGTERM", () => {
+    gracefulShutdown("Heroku app shutdown", () => {
         process.exit(0);
     });
 });
@@ -64,4 +64,4 @@ process.on('SIGINT', () => {
 connect();
 
 // bring in the Mongoose schema 
-require('./travlr');
+require("./models/travlr");
