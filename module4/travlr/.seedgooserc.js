64ee36dc91ebe19590cbb120a88392256ef73fbc
kglass9 <107563649+kglass9@users.noreply.github.com>
2023-09-28 21:22:22 -0400

    Delete module4/travlr directory

--- module4/travlr/.seedgooserc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    modelBaseDirectory: "app_server/database/models",
-    models: ["*.js","!db.js"],
-    data: 'data',
-    db: 'mongodb://localhost:27017/travlr'
-  };
--- module4/travlr/app_server/routes/news.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller= require('../controllers/news');
-
-/* GET news page. */
-router.get('/', controller.news);
-
-module.exports = router;
