ad724e24b2c0649f6222414f4ccea66f31a33d5b
kglass9 <107563649+kglass9@users.noreply.github.com>
2023-10-06 11:25:43 -0400

    Delete module6/travlr directory

--- module6/travlr/app_server/routes/about.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller= require('../controllers/about');
-
-/* GET about page. */
-router.get('/', controller.about);
-
-module.exports = router;
